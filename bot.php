3b95e6a64992cc7818edfbbcd420a655e3b7bfe9
emrizaal <emrizal21@gmail.com>
2016-10-14 08:07:48 +0700

    1st commit

---
+++ bot.php
@@ -0,0 +1,125 @@
+<div id="page-up" class="div4"><span class="up"><a href="#top">PAGE UP</a></span></div>
+
+      <!-- =========== footer =========== -->
+      <div id="footer" class="div4">
+        <div class="wrap">
+          <div class="footnewsmail"><div class="padLR30">
+            <p>Receive updates, events and more to your inbox.</p>
+            <span class="button-form"><a class="sign fancybox" data-fancybox-type="iframe" href="http://saujana.com.my/newsmail.php">SIGN UP FOR NEWSMAIL</a></span>
+          </div></div>
+          <div class="footmenu">
+            <div class="link">
+              <span class="f12 ls1 fw">MEMBERSHIP</span>
+              <p class="t12 fw"><a href="http://saujana.com.my/members_general.php">General information on our membership</a></p>
+            </div>
+            <div class="link">
+              <span class="f12 ls1 fw">PROMOTIONS</span>
+              <p class="t13 fw"><a href="http://saujana.com.my/rates_golf.html">Golf Packages &amp; F&amp;B </a></p>
+            </div>
+            <div class="link">
+              <span class="f12 ls1 fw">FACTSHEET</span>
+              <p class="t12 fw"><a href="http://saujana.com.my/doc/SGCC_Factsheet.pdf" target="_blank">Download the club's factsheet</a></p>
+            </div>
+            <div class="link">
+              <span class="f12 ls1 fw">HOTEL</span>
+              <p class="t12 fw"><a href="http://saujana.com.my/hotel_thesaujana.html">Bandung Giri Gahana Hotel</a></p>
+              <p class="t12 fw"><a href="http://saujana.com.my/hotel_theclub.html">Bandung Giri Gahana Resort</a></p>
+              <p class="t12 fw"><a href="http://saujana.com.my/hotel_thespa.html">The Spa, The Club Bandung Giri Gahana Resort</a></p>
+            </div>
+            <div class="link">
+              <span class="f12 ls1 fw">BOOKING</span>
+              <p class="t12 fw"><a href="http://www.saujanaonline.com.my/" target="_blank">Make your reservations online now</a></p>
+              <p class="t12 fw"><a href="http://saujana.com.my/doc/SGCC_Booking_Form.pdf" target="_blank">Booking Form</a></p>
+            </div>
+          </div>
+
+          <div class="clear"></div>
+
+          <div class="clear"></div>
+
+          <div class="misc">
+            <!--<div class="col-partners"></div>-->
+            <div class="col-social"><!-- AddThis Button BEGIN -->
+              <table width="100%" border="0" cellspacing="0" cellpadding="0">
+                <tbody><tr>
+
+                </tr>
+                <tr>
+                  <td valign="top"><div class="miscmenu"><ul><li><a href="http://saujana.com.my/inquiry.html">Inquiry</a></li><li><a href="http://saujana.com.my/terms.html">Terms &amp; Conditions</a></li>
+                  </ul></div></td>
+                </tr>
+              </tbody></table>
+              <!-- AddThis Button END --></div><div class="clear"></div></div><p class="disclaimer">© Copyright 2016.</p>
+            </div>
+          </div>
+
+          <!-- =========== end footer =========== -->
+
+
+
+
+
+
+
+
+
+          <script src="assets/js/cbpHorizontalMenu.js"></script>
+          <script>
+            $(function() {
+             cbpHorizontalMenu.init();
+           });
+         </script>
+
+
+
+         <script type="text/javascript">
+          $(function() {
+            $('div#page-up a').bind('click',function(event){
+              var $anchor = $(this);
+
+              $('html, body').stop().animate({
+                scrollTop: $($anchor.attr('href')).offset().top
+              }, 1000,'easeInOutExpo');
+              event.preventDefault();
+            });
+          });
+
+        </script>
+        <script type="text/javascript">
+          $(function() {
+            $('div#page-down a').bind('click',function(event){
+              var $anchor = $(this);
+
+              $('html, body').stop().animate({
+                scrollTop: $($anchor.attr('href')).offset().top
+              }, 1000,'easeInOutExpo');
+              event.preventDefault();
+            });
+          });
+
+        </script>
+
+        <script type="text/javascript">
+          $(function() {
+            $('div.discover a').bind('click',function(event){
+              var $anchor = $(this);
+
+              $('html, body').stop().animate({
+                scrollTop: $($anchor.attr('href')).offset().top
+              }, 1000,'easeInOutExpo');
+                    /*
+                    if you don't want to use the easing effects:
+                    $('html, body').stop().animate({
+                        scrollTop: $($anchor.attr('href')).offset().top
+                    }, 1000);
+                    */
+                    event.preventDefault();
+                  });
+          });
+
+        </script>
+
+
+
+
+      </body></html>
---
+++ top.php
@@ -0,0 +1,238 @@
+<!DOCTYPE html>
+<!-- saved from url=(0022)http://saujana.com.my/ -->
+<html xmlns:fb="http://ogp.me/ns/fb#" class="js wf-trajanpro3-n6-active wf-trajanpro3-n7-active wf-active"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+<title>Bandung Giri Gahana Golf and Resort</title>
+
+<script id="facebook-jssdk" src="assets/js/all.js"></script>
+<script src="assets/js/jquery.js"></script>
+<script type="text/javascript" src="assets/js/jquery.easing.1.3.js"></script>
+<script src="assets/js/modernizr.custom.js"></script>
+<script type="text/javascript" src="assets/js/pgu6lcb.js"></script>
+<script type="text/javascript" src="assets/js/vticker.js"></script>
+
+<style type="text/css">.tk-trajan-pro-3{font-family:"trajan-pro-3",sans-serif;}</style><style type="text/css">@font-face{font-family:trajan-pro-3;src:url(https://fonts.typekit.net/af/eb79d7/000000000000000000012e8f/27/l?subset_id=2&fvd=n7) format("woff2");font-weight:700;font-style:normal;}@font-face{font-family:trajan-pro-3;src:url(https://fonts.typekit.net/af/a6c357/000000000000000000012e93/27/l?subset_id=2&fvd=n6) format("woff2");font-weight:600;font-style:normal;}</style>
+<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
+<link href="assets/css/css.css" rel="stylesheet" type="text/css">
+
+<link href="assets/css/sau_general.css" rel="stylesheet" type="text/css">
+<link href="assets/css/sau_response.css" rel="stylesheet" type="text/css">
+
+<link rel="stylesheet" type="text/css" href="assets/css/component_menu.css">
+<link rel="stylesheet" type="text/css" href="assets/css/slicknav.css">
+<script src="assets/js/jquery.slicknav.js"></script>
+<script type="text/javascript" src="assets/js/jquery.fancybox.js"></script>
+<link rel="stylesheet" type="text/css" href="assets/css/fancybox.css" media="screen">
+<script type="text/javascript">
+  $(document).ready(function(){
+   $('#mobilemenu').slicknav();
+
+
+   Resize();
+
+   $(".sign").fancybox({
+    autoSize   : 'true',
+    width  : '400',
+    height : '350',
+    maxWidth  : '400',
+    maxHeight : '380'
+  });
+
+ });
+  $(window).resize(function() {
+    Resize();
+  });
+
+
+  function Resize() {
+
+    var windowHeight = $(window).height() + 'px';
+    $('.slides').css('height', windowHeight);
+  }
+
+  $(function() {
+    $('#ticker').vTicker();
+  });
+</script>
+
+<style type="text/css">.fancybox-margin{margin-right:17px;}</style>
+<link rel="stylesheet" type="text/css" href="assets/css/top.css">
+<link rel="shortcut icon" href="favicon.png">
+</style></head><body>
+
+<!-- =========== top =========== -->
+
+<div id="mb-header"><a href="http://saujana.com.my/index.php"></a></div>
+<div id="mb-menu"><div class="slicknav_menu"><a href="http://saujana.com.my/#" aria-haspopup="true" tabindex="0" class="slicknav_btn" style=""><span class="slicknav_menutxt">NAVIGATE</span><span class="slicknav_icon"><span class="slicknav_icon-bar"></span><span class="slicknav_icon-bar"></span><span class="slicknav_icon-bar"></span></span></a><ul id="mobilemenu" class="slicknav_nav slicknav_hidden" style="display: none;" aria-hidden="true" role="menu">
+  <li class="slicknav_collapsed slicknav_parent"><a href="http://saujana.com.my/#" role="menuitem" aria-haspopup="true" tabindex="-1" class="slicknav_item" style="">CLUB<span class="slicknav_arrow"> ►</span></a><ul role="menu" class="slicknav_hidden" style="display: none;" aria-hidden="true">
+    <li><a href="http://saujana.com.my/club_history.html" class="navlink" role="menuitem" tabindex="-1">History &amp; Board Of Directors</a></li>
+    <li><a href="http://saujana.com.my/club_accolades.php" class="navlink" role="menuitem" tabindex="-1">Accolades &amp; Host Venue</a></li>
+    <li><a href="http://saujana.com.my/club_facilities.html" class="navlink" role="menuitem" tabindex="-1">Facilities</a></li>
+    <li><a href="http://saujana.com.my/club_csr.php" class="navlink" role="menuitem" tabindex="-1">Corporate Social Responsibility</a></li>
+    <li><a href="http://saujana.com.my/club_academy.html" class="navlink" role="menuitem" tabindex="-1">Golf Academy</a></li>
+    <li><a href="http://saujana.com.my/club_news.php" class="navlink" role="menuitem" tabindex="-1">News Updates</a></li>
+  </ul> </li>
+
+  <li class="slicknav_collapsed slicknav_parent"><a href="http://saujana.com.my/#" role="menuitem" aria-haspopup="true" tabindex="-1" class="slicknav_item" style="">COURSES<span class="slicknav_arrow"> ►</span></a><ul role="menu" class="slicknav_hidden" style="display: none;" aria-hidden="true">
+    <li><a href="http://saujana.com.my/course_palm.html" class="navlink" role="menuitem" tabindex="-1">Palm Course</a></li>
+    <li><a href="http://saujana.com.my/course_bungaraya.html" class="navlink" role="menuitem" tabindex="-1">Bunga Raya Course</a></li>
+    <li><a href="http://saujana.com.my/course_conditions.php" class="navlink" role="menuitem" tabindex="-1">Course Conditions</a></li>
+  </ul> </li>
+
+  <li class="slicknav_collapsed slicknav_parent"><a href="http://saujana.com.my/#" role="menuitem" aria-haspopup="true" tabindex="-1" class="slicknav_item" style="">MEMBERSHIP<span class="slicknav_arrow"> ►</span></a><ul role="menu" class="slicknav_hidden" style="display: none;" aria-hidden="true">
+    <li><a href="http://saujana.com.my/members_general.php" class="navlink" role="menuitem" tabindex="-1">General Information</a></li>
+    <li><a href="http://saujana.com.my/members_reciprocal.php" class="navlink" role="menuitem" tabindex="-1">Reciprocal Clubs</a></li>
+    <li><a href="http://saujana.com.my/members_checklist.html" class="navlink" role="menuitem" tabindex="-1">Application Checklist</a></li>
+  </ul> </li>
+
+  <li class="slicknav_collapsed slicknav_parent"><a href="http://saujana.com.my/#" role="menuitem" aria-haspopup="true" tabindex="-1" class="slicknav_item" style="">RATES<span class="slicknav_arrow"> ►</span></a><ul role="menu" class="slicknav_hidden" style="display: none;" aria-hidden="true">
+    <li><a href="http://saujana.com.my/rates_golf.php" class="navlink" role="menuitem" tabindex="-1">Golf Rates &amp; Promotions</a></li>
+    <li><a href="http://saujana.com.my/rates_sport.php" class="navlink" role="menuitem" tabindex="-1">Sports Rates</a></li>
+    <li><a href="http://saujana.com.my/terms.html" class="navlink" role="menuitem" tabindex="-1">Terms &amp; Conditions</a></li>
+  </ul> </li>
+
+  <li class="slicknav_collapsed slicknav_parent"><a href="http://saujana.com.my/#" role="menuitem" aria-haspopup="true" tabindex="-1" class="slicknav_item" style="">EVENTS<span class="slicknav_arrow"> ►</span></a><ul role="menu" class="slicknav_hidden" style="display: none;" aria-hidden="true">
+    <li><a href="http://saujana.com.my/event_calendar.html" class="navlink" role="menuitem" tabindex="-1">Event Calendar</a></li>
+    <li><a href="http://saujana.com.my/event_orderofmerit.php" class="navlink" role="menuitem" tabindex="-1">Order of Merit &amp; Monthly Medal</a></li>
+    <li><a href="http://saujana.com.my/event_results.php" class="navlink" role="menuitem" tabindex="-1">Competition Results</a></li>
+  </ul> </li>
+
+  <li><a href="http://saujana.com.my/horizon.html" role="menuitem" tabindex="-1">HORIZON</a></li>
+
+  <li class="slicknav_collapsed slicknav_parent"><a href="http://saujana.com.my/#" role="menuitem" aria-haspopup="true" tabindex="-1" class="slicknav_item" style="">HOTEL<span class="slicknav_arrow"> ►</span></a><ul role="menu" class="slicknav_hidden" style="display: none;" aria-hidden="true">
+    <li><a href="http://saujana.com.my/hotel_thesaujana.html" class="navlink" role="menuitem" tabindex="-1">Bandung Giri Gahana Hotel</a></li>
+    <li><a href="http://saujana.com.my/hotel_theclub.html" class="navlink" role="menuitem" tabindex="-1">Bandung Giri Gahana Resort</a></li>
+    <li><a href="http://saujana.com.my/hotel_thespa.html" class="navlink" role="menuitem" tabindex="-1">The Spa, The Club Bandung Giri Gahana Resort</a></li>
+  </ul> </li>
+
+  <li><a href="http://saujana.com.my/pressmedia.php" role="menuitem" tabindex="-1">PRESS &amp; MEDIA</a></li>
+
+  <li class="slicknav_collapsed slicknav_parent"><a href="http://saujana.com.my/#" role="menuitem" aria-haspopup="true" tabindex="-1" class="slicknav_item" style="">CONTACTS<span class="slicknav_arrow"> ►</span></a><ul role="menu" class="slicknav_hidden" style="display: none;" aria-hidden="true">
+    <li><a href="http://saujana.com.my/contacts_management.php" class="navlink" role="menuitem" tabindex="-1">Management &amp; Committees</a></li>
+    <li><a href="http://saujana.com.my/contacts_location.html" class="navlink" role="menuitem" tabindex="-1">Location</a></li>
+    <!--<li><a href="inquiry.html" class="navlink">Inquiry</a></li>-->
+  </ul> </li>
+
+  <li><a href="http://www.saujanaonline.com.my/" target="_blank" role="menuitem" tabindex="-1">ONLINE BOOKING</a></li>
+
+</ul></div>
+
+
+</div><!-- End mobile menu -->
+
+
+
+<!-- =========== top =========== -->
+
+<div id="sticky"><div class="wrap-menu">
+  <div id="idfull"><a href="http://saujana.com.my/index.php"></a></div>
+  <div id="register"><ul><li><a href="http://www.saujanaonline.com.my/" target="_blank">Members Login</a></li></ul></div>
+  <div id="cbp-hrmenu" class="cbp-hrmenu">
+    <ul>
+
+      <li>
+        <a href="http://saujana.com.my/#">CLUB</a>
+        <div class="cbp-hrsub"><div class="cbp-hrsub-inner"><div class="wrapsub">
+          <div class="menudesc">A golfing excursion in Malaysia is never complete without a visit to Bandung Giri Gahana Golf &amp; Country Club.</div>
+          <div class="col sub20 border"><a href="http://saujana.com.my/club_history.html"><h4>History &amp; Board Of Directors</h4></a><h5>The first proprietary club in the country.</h5></div>
+          <div class="col sub20 border"><a href="http://saujana.com.my/club_accolades.php"><h4>Accolades &amp; Host Venue</h4></a><h5>World wide recognition over the years.</h5></div>
+          <div class="col sub20 border"><a href="http://saujana.com.my/club_facilities.html"><h4>Facilities</h4></a><h5>The club offers more than just golf.</h5></div>
+          <div class="col sub20 border"><a href="http://saujana.com.my/club_csr.php"><h4>CSR</h4></a><h5>Community driven activities &amp; initiatives.</h5></div>
+          <div class="col sub20 border"><a href="http://saujana.com.my/club_academy.html"><h4>Golf Academy</h4></a><h5>Golf lessons by the Pros.</h5></div>
+          <div class="col subbottom"><span class="copy">Make your tee-off reservation now</span><a href="http://www.saujanaonline.com.my/" target="_blank"><span class="button-menu">ONLINE BOOKING</span></a><a href="http://saujana.com.my/club_news.php"><h4 class="floatR">NEWS UPDATES</h4></a></div>
+          <div class="clear"></div></div></div><!-- /cbp-hrsub-inner --></div>
+          <!-- /cbp-hrsub -->
+        </li>
+
+        <li>
+          <a href="http://saujana.com.my/#">COURSES</a>
+          <div class="cbp-hrsub"><div class="cbp-hrsub-inner"><div class="wrapsub">
+            <div class="col subdesc-L"><p class="menudesc">Bandung Giri Gahana Golf &amp; Country Club offers two championship golf courses, the 'Palm Course' and the 'Bunga Raya Course'.</p></div>
+            <div class="col sub60 border"><table width="100%" border="0" cellspacing="0" cellpadding="0">
+              <tbody><tr>
+                <td width="50%" valign="top"><div class="img-t-coursepalm"></div></td>
+                <td width="50%" class="border"><div class="img-t-coursebunga"></div></td>
+              </tr>
+              <tr>
+                <td valign="top"><a href="http://saujana.com.my/course_palm.html"><h4>Palm Course</h4></a></td>
+                <td valign="top"><a href="http://saujana.com.my/course_bungaraya.html"><h4>Bunga Raya Course</h4></a></td>
+              </tr>
+              <tr>
+                <td valign="top"><h5>Carved out of an oil palm plantation, the Palm Course boasts of a rugged terrain, with slopes which drop to steep ravines.</h5></td>
+                <td valign="top"><h5>Consisting of a wide expanse of water and undulating greenery that creates a sense of freedom and space.</h5></td>
+              </tr>
+              <tr>
+                <td valign="top"><a href="http://saujana.com.my/course_palm1.html"><h4>Hole Descriptions</h4></a></td>
+                <td valign="top"><a href="http://saujana.com.my/course_br1.html"><h4>Hole Descriptions</h4></a></td>
+              </tr>
+            </tbody></table>
+          </div>
+          <div class="col sub20 border">
+            <ul class="menulist">
+              <li><a href="http://saujana.com.my/course_conditions.php"><h4>Course Condition</h4></a></li>
+              <li><a href="http://saujana.com.my/course_scorecard.html"><h4>Score Card</h4></a></li></ul></div>
+              <div class="col subbottom"><span class="copy">Make your tee-off reservation now</span><a href="http://www.saujanaonline.com.my/" target="_blank"><span class="button-menu">ONLINE BOOKING</span></a></div>
+              <div class="clear"></div></div></div><!-- /cbp-hrsub-inner --></div>
+              <!-- /cbp-hrsub -->
+            </li>
+
+            <li>
+              <a href="http://saujana.com.my/#">MEMBERSHIP</a>
+              <div class="cbp-hrsub"><div class="cbp-hrsub-inner"><div class="wrapsub">
+                <div class="menudesc">Club membership offer you more benefits and privilleges.</div>
+                <div class="col sub30 border"><a href="http://saujana.com.my/members_general.php"><h4>General Information</h4></a><h5>Overview of Saujana Golf &amp; Country Club Membership.</h5><a href="http://saujana.com.my/mime/SGCC_Membership_GeneralInfo.pdf" target="_blank"><h6>Download document</h6></a></div>
+                <div class="col sub30 border"><a href="http://saujana.com.my/members_reciprocal.php"><h4>Reciprocal Clubs</h4></a><h5>List of our worldwide reciprocal clubs.</h5><a href="http://saujana.com.my/doc/SGCC_Membership_Reciprocal.pdf" target="_blank"><h6>Download document</h6></a></div>
+                <div class="col sub30 border"><a href="http://saujana.com.my/members_checklist.html"><h4>Application Checklist</h4></a><h5>Checklist in preparing your membership application.</h5><a href="http://saujana.com.my/mime/Membership%20Checklist%20For%20Member.pdf" target="_blank"><h6>Download document</h6></a></div>
+                <div class="clear"></div></div></div><!-- /cbp-hrsub-inner --></div>
+                <!-- /cbp-hrsub -->
+              </li>
+
+              <li>
+                <a href="http://saujana.com.my/#">RATES</a>
+                <div class="cbp-hrsub"><div class="cbp-hrsub-inner"><div class="wrapsub">
+                  <div class="col subauto border"><a href="http://saujana.com.my/rates_golf.php"><h4>Golf Rates &amp; Promotions</h4></a></div>
+                  <div class="col subauto border"><a href="http://saujana.com.my/rates_sport.php"><h4>Sports Rates</h4></a></div>
+                  <div class="col subauto border"><a href="http://saujana.com.my/terms.html"><h4>Terms &amp; Conditions</h4></a></div>
+                  <div class="clear"></div></div></div><!-- /cbp-hrsub-inner --></div>
+                  <!-- /cbp-hrsub -->
+                </li>
+
+                <li>
+                  <a href="http://saujana.com.my/#">EVENTS</a>
+                  <div class="cbp-hrsub"><div class="cbp-hrsub-inner"><div class="wrapsub">
+                    <div class="menudesc">Home of great tournaments and championships.</div>
+                    <div class="col sub30 border"><a href="http://saujana.com.my/event_calendar.html"><h4>Event Calendar</h4></a><h5>Checkout all the events happening at the club.</h5></div>
+                    <div class="col sub30 border"><a href="http://saujana.com.my/event_orderofmerit.php"><h4>Order Of Merit &amp; Monthly Medals</h4></a><h5>Golfers standing - Mens and Ladies.</h5></div>
+                    <div class="col sub30 border"><a href="http://saujana.com.my/event_results.php"><h4>Competition Results</h4></a><h5>Recent and past tournament &amp; championship winners.</h5></div>
+                    <div class="clear"></div></div></div><!-- /cbp-hrsub-inner --></div>
+                    <!-- /cbp-hrsub -->
+                  </li>
+
+                  <li>
+                    <a href="http://saujana.com.my/#">HOTEL</a>
+                    <div class="cbp-hrsub"><div class="cbp-hrsub-inner"><div class="wrapsub">
+                      <div class="menudesc">Saujana Golf &amp; Country Club is nestled alongside two hotels, namely The Saujana Hotel Kuala Lumpur and The Club Saujana Resort. Managed by Saujana Hotels &amp; Resorts, the prestigious heritage mansion, Carcosa Seri Negara also falls under their portfolio.</div>
+                      <div class="col sub30 border"><a href="http://saujana.com.my/hotel_thesaujana.html"><h4>The Saujana Hotel Kuala Lumpur</h4></a><h5>Scenic beauty and unconditional privacy abound, The Saujana combines comfort and adventure of lush tropical garden.</h5><a href="http://www.thesaujanahotel.com/" target="_blank"><h6>www.thesaujanahotel.com</h6></a></div>
+                      <div class="col sub30 border"><a href="http://saujana.com.my/hotel_theclub.html"><h4>The Club Saujana Resort</h4></a><h5>Ultimate urban retreat offering complete luxury and personalised services.</h5><a href="http://www.theclubsaujanaresort.com/" target="_blank"><h6>www.theclubsaujanaresort.com</h6></a></div>
+                      <div class="col sub30 border"><a href="http://saujana.com.my/hotel_thespa.html"><h4>The Spa, The Club Saujana Resort</h4></a><h5>Dedicated to the restoration of strength, beauty, balance of mind, body and soul.</h5><a href="http://www.thespa-theclubsaujanaresort.com/" target="_blank"><h6>www.thespa-theclubsaujanaresort.com</h6></a></div>
+                      <div class="clear"></div></div></div><!-- /cbp-hrsub-inner --></div>
+                      <!-- /cbp-hrsub -->
+                    </li>
+
+                    <li><a onclick="window.location.href=&#39;horizon.html&#39;;return false;" href="http://saujana.com.my/horizon.html">HORIZON</a></li>
+                    <li><a onclick="window.location.href=&#39;pressmedia.php&#39;;return false;" href="http://saujana.com.my/pressmedia.php">PRESS &amp; MEDIA</a></li>
+
+                    <li>
+                      <a href="http://saujana.com.my/#">CONTACTS</a>
+                      <div class="cbp-hrsub"><div class="cbp-hrsub-inner"><div class="wrapsub">
+                        <div class="col subauto border"><a href="http://saujana.com.my/contacts_management.php"><h4>Management &amp; Committees</h4></a></div>
+                        <div class="col subauto border"><a href="http://saujana.com.my/contacts_location.html"><h4>Location</h4></a></div>
+                        <!--<div class="col subauto border"><a href="inquiry.html"><h4>Inquiry</h4></a></div>-->
+                        <div class="clear"></div></div></div><!-- /cbp-hrsub-inner --></div>
+                        <!-- /cbp-hrsub -->
+                      </li>
+
+
+                    </ul></div><!-- =========== end menu =========== -->
+                  </div></div>
+                  <!-- =========== end top =========== -->
