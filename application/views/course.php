5cbd231b1b257c424c1b4321ce907ceacfa4de94
Taofik R <taofik.wirahmana@gmail.com>
2016-10-21 16:17:47 +0700

    mobile responsive

--- application/views/course.php
+++ application/views/course.php
@@ -1,56 +1,62 @@
 <?php include "top.php" ?>
 <div id="ticker" style="overflow: hidden; position: relative; height: 24px;"></div>
-<div style="margin-top:100px;background-image:url('../assets/image/cover_palm.jpg');height:600px;">
+<div class="div1">
+<div class="bg-course">
 
-	<div class="row-section" style="background-color:black;opacity:0.8;color:white;bottom:-50px;position:absolute;">
-		<div class="page-section mar-T20"><h1>PALM COURSE</h1></div>
-		<h2 class="tcen">Ti Ratana Community Centre</h2>
+	<div class="row-section">
+        <div class="dark">
+            <div class="page-section mar-T20"><h1>PALM COURSE</h1></div>
+            <h2 class="tcen">Ti Ratana Community Centre</h2>
 
 
-		<div class="block-48-L border-R"><div class="pad-20">
-			<p class="tjus">A centre of hope, sharing and growth that caters to the needs of children, young people and senior citizens, regardless of race or creed. In these times of haste, stress and pressure, we have created Ti-Ratana to be a sanctuary, a centre of hope for the hopeless and the helpless, a place of rest for the infirm or the healthy, a place to learn and grow, love, share or just be.The total money raised amounted to more than RM530,000 this year compared with RM415,000 previously in this annual charity golf game in aid of the society. A buffet dinner-cum-cheque presentation ceremony was held at the Ti Chen Chinese Restaurant at the Saujana Golf and Country Club after players had their round of play at the golf course earlier in the afternoon. Organising committee co-chairman Tan Chuan Yong said from the total of RM530,000, more than RM200,000 were through fees of flights by the players and also through sponsorship of the souvenir programme book.</p>
-		</div></div>
-		<center>
-			<table width="70%" border="0" cellspacing="0" cellpadding="0">
-				<tbody>
-					<tr">
-					<td width="50%" valign="top" class="t15" style="border-bottom:1px solid #C2A13D"><span class="fgo">Designer:</span> Ronald Fream</td>
-					<td width="50%" valign="top" style="border-bottom:1px solid #C2A13D">&nbsp;</td>
-				</tr>
-				<tr>
-					<td valign="top" style="border-bottom:1px solid #C2A13D"><span class="t13 fgo ls1">GRASS</span></td>
-					<td valign="top" style="border-bottom:1px solid #C2A13D"><span class="t13 fgo ls1">COURSE SLOPE RATINGS</span></td>
-				</tr>
-				<tr>
-					<td valign="top"><table width="100%" border="0" cellspacing="0" cellpadding="0" class="tdno">
-						<tbody><tr>
-							<td width="33%" valign="top" style="border-bottom:1px solid #C2A13D"><span class="fgo">Green:</span><br>Tifdwarf Bermuda</td>
-							<td width="33%" valign="top" style="border-bottom:1px solid #C2A13D"><span class="fgo">Fairway &amp; Tees:</span><br>Bermuda</td>
-							<td width="34%" valign="top" style="border-bottom:1px solid #C2A13D"><span class="fgo">Length:</span><br>
-								6,992 yards</td>
-							</tr>
-						</tbody>
-					</table>
-					</td>
-					<td valign="top"><table width="100%" border="0" cellspacing="0" cellpadding="0" class="tdno">
-						<tbody><tr>
-							<td width="25%" valign="top" style="border-bottom:1px solid #C2A13D"><span class="fgo">Black Tees:</span><br>
-								75.1 / 142</td>
-								<td width="25%" valign="top" style="border-bottom:1px solid #C2A13D"><span class="fgo">Blue Tees:</span><br>
-									73.0 / 140</td>
-									<td width="25%" valign="top" style="border-bottom:1px solid #C2A13D"><span class="fgo">White Tees:</span><br>
-										70.3 / 137</td>
-										<td width="25%" valign="top" style="border-bottom:1px solid #C2A13D"><span class="fgo">Red Tees:</span><br>
-											73.1 / 132</td>
-										</tr>
-									</tbody></table></td>
-								</tr>
-								<tr>
-									<td colspan="2" style="border-bottom:1px solid #C2A13D"><span class="t13">Note: The information provided herewith is accurate at time of publication only. The layout and other information is subject to changes as and when the Board of Directors &amp; Designers deem fit and necessary.</span></td>
-								</tr>
-							</tbody>
-						</table>
-					</center>
-				</div>
-			</div>
-			<?php include "bot.php" ?>
+            <div class="block-48-L border-R"><div class="pad-20">
+                <p class="tjus">A centre of hope, sharing and growth that caters to the needs of children, young people and senior citizens, regardless of race or creed. In these times of haste, stress and pressure, we have created Ti-Ratana to be a sanctuary, a centre of hope for the hopeless and the helpless, a place of rest for the infirm or the healthy, a place to learn and grow, love, share or just be.The total money raised amounted to more than RM530,000 this year compared with RM415,000 previously in this annual charity golf game in aid of the society. A buffet dinner-cum-cheque presentation ceremony was held at the Ti Chen Chinese Restaurant at the Saujana Golf and Country Club after players had their round of play at the golf course earlier in the afternoon. Organising committee co-chairman Tan Chuan Yong said from the total of RM530,000, more than RM200,000 were through fees of flights by the players and also through sponsorship of the souvenir programme book.</p>
+            </div></div>
+            <center>
+                <table width="70%" border="0" cellspacing="0" cellpadding="0">
+                    <tbody>
+                        <tr>
+                        <td width="50%" valign="top" class="t15" style="border-bottom:1px solid #C2A13D"><span class="fgo">Designer:</span> Ronald Fream</td>
+                        <td width="50%" valign="top" style="border-bottom:1px solid #C2A13D">&nbsp;</td>
+                    </tr>
+                    <tr>
+                        <td valign="top" style="border-bottom:1px solid #C2A13D"><span class="t13 fgo ls1">GRASS</span></td>
+                        <td valign="top" style="border-bottom:1px solid #C2A13D"><span class="t13 fgo ls1">COURSE SLOPE RATINGS</span></td>
+                    </tr>
+                    <tr>
+                        <td valign="top"><table width="100%" border="0" cellspacing="0" cellpadding="0" class="tdno">
+                            <tbody><tr>
+                                <td width="33%" valign="top" style="border-bottom:1px solid #C2A13D"><span class="fgo">Green:</span><br>Tifdwarf Bermuda</td>
+                                <td width="33%" valign="top" style="border-bottom:1px solid #C2A13D"><span class="fgo">Fairway &amp; Tees:</span><br>Bermuda</td>
+                                <td width="34%" valign="top" style="border-bottom:1px solid #C2A13D"><span class="fgo">Length:</span><br>
+                                    6,992 yards</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                        </td>
+                        <td valign="top"><table width="100%" border="0" cellspacing="0" cellpadding="0" class="tdno">
+                            <tbody><tr>
+                                <td width="25%" valign="top" style="border-bottom:1px solid #C2A13D"><span class="fgo">Black Tees:</span><br>
+                                    75.1 / 142</td>
+                                    <td width="25%" valign="top" style="border-bottom:1px solid #C2A13D"><span class="fgo">Blue Tees:</span><br>
+                                        73.0 / 140</td>
+                                        <td width="25%" valign="top" style="border-bottom:1px solid #C2A13D"><span class="fgo">White Tees:</span><br>
+                                            70.3 / 137</td>
+                                            <td width="25%" valign="top" style="border-bottom:1px solid #C2A13D"><span class="fgo">Red Tees:</span><br>
+                                                73.1 / 132</td>
+                                            </tr>
+                                        </tbody></table></td>
+                                    </tr>
+                                    <tr>
+                                        <td colspan="2" style="border-bottom:1px solid #C2A13D"><span class="t13">Note: The information provided herewith is accurate at time of publication only. The layout and other information is subject to changes as and when the Board of Directors &amp; Designers deem fit and necessary.</span></td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </center>
+                </div>
+            </div>
+    <div class="clear"></div>
+        </div>
+    </div>
+
+<?php include "bot.php" ?>
